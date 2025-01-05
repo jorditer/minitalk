1d983dd8db474db7ec3f3575ea4ba17b188723c8
jorditer <jordi.terradas6@gmail.com>
2025-01-05 22:34:57 +0100

    stable

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: jordi <jordi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 11:02:54 by anlima            #+#    #+#             */
-/*   Updated: 2025/01/05 22:24:26 by jordi            ###   ########.fr       */
+/*   Updated: 2025/01/05 22:34:12 by jordi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int	main(void)
 	}
 	write(1, "\e[4:1mJelou! Vamos a ir empezando la correccion...\e[4:0m\n", 58);
 	// write(1, "\e[4:3mWelcome to anlima's minitalk project!\e[4:0m\n", 51);
-	write(1, "Server ready, PID = ", 19);
+	write(1, "Server ready, PID = ", 20);
 	ft_putnbr(pid);
 	write(1, "\n", 1);
 	while (1)
